39b36d472343f3af9e1f71a819b3f89d3bb92b3c
leilanivquesada <62270422+leilanivquesada@users.noreply.github.com>
2021-04-19 15:59:44 -0700

    completed assignments
    Temperature and Calculator

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
-#write your code here
+def ftoc(fahrenheit)
+	
+	 ((fahrenheit - 32) * 5 / 9).round(2)
+end
+
+def ctof(celsius)
+	((celsius * 9 / 5.0)+ 32).round(2)
+
+end
+
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,22 @@
-#write your code here
+def sum (input)
+	total = 0
+	if input.length() > 0
+		input.each {|item| 
+    total += item}
+    return total
+	else 
+		total = 0 
+  end
+end
+
+#iterate over the input to add all the input items together
+
+
+def subtract (num1, num2)
+	num1.to_f - num2.to_f
+end
+
+def add (num1, num2)
+	num1.to_f + num2.to_f
+end
+
