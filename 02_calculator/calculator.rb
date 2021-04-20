05b101562e10147d98f370e461334d6e8657a3dd
Rob Jacobson <rob@ymail.com>
2021-04-19 17:34:21 -0700

    calculator passes all tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(arr)
+  arr.reduce(0) { |sum, num| sum + num}
+end
+
+def multiply(*numbers) 
+  numbers.reduce(1) { |result, num| result * num }
+end
+
+def power(a, b)
+  a ** b
+end
