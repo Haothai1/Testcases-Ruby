4e92a0af5a599a7ce3a014108282cb2676f3dd5b
leilanivquesada <62270422+leilanivquesada@users.noreply.github.com>
2021-04-21 06:22:15 -0700

    completed book titles problem

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
+	attr_accessor :title 
+
+
+
+
+	def title (title)
+		title.split.map(&:capitalize).join(' ')
+	end
 # write your code here
 end
+
+
+
+conjunctions = ['at', 'by', 'of', 'on', 'to', 'onto', ]
