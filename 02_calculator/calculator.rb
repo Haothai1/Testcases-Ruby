a254060cac9163e8c317802d57d047800c821bbd
CeeFish <can.o.fisher@gmail.com>
2022-04-26 12:15:59 -0400

    All test pass in the calculator file.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+    add = num1 + num2
+end
+
+add(2, 2)
+
+def subtract(num3, num4)
+    subtract = num3 -num4
+end
+
+subtract(6, 3)
+
+def sum(array)
+    array.inject(0){|sum, array| sum += array }
+end
