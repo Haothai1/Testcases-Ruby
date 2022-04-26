249e7cf64a4be03c275bcac1cae8d8b12b22a5ae
CeeFish <can.o.fisher@gmail.com>
2022-04-26 12:15:00 -0400

    All test pass in the temperature file.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+def ftoc(fahrenheit)
+    celsius = (fahrenheit.to_i - 32) * 5.0/9.0
+    celsius.round(1).ceil
+end
+
+def ctof(celsius)
+    fahrenheit = (celsius.to_i * 9.0/5.0) + 32
+    fahrenheit.round(1)
+end
