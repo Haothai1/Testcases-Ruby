5183b6f5e28be19e265a7855da5f71f4839c0c5c
CeeFish <can.o.fisher@gmail.com>
2022-04-28 11:15:57 -0400

    The first part of the test in pig latin passes.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+class PigLatin
+    
+    def pig_latin(words)
+        first_step = words.split(" ")
+        second_step = a.map {|string| pig_latin_word(string)}
+        second_step.join(" ")
+    end
+
+    def translate (string)
+        first_letter = string[0].downcase
+        consonants = []
+
+        if ["a", "e", "i", "o", "u"].include?(first_letter)
+            return "#{string}ay"
+        else 
+            return if ["a", "e", "i", "o", "u"].include?(string[1, 2]) == false
+        end
+    "#{string[consonants.length..-1] + consonants.join + "ay"}"
+    end
+end
+
