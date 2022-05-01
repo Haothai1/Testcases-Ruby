75cb5283ca460bd9c8f206b1a55892c270bd275d
CeeFish <can.o.fisher@gmail.com>
2022-05-01 15:31:57 -0400

    Timer file seems to be working.

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def seconds
+    @seconds = 0
+  end
+
+  def time_string
+    time = Time.new("%H:%M:%S")
+    time.strftime("%H:%M:%S")
+
+    hours = @seconds/3600
+    remainder = @seconds%3600
+    sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+  end
 end
+
+
+
