cccf10bcfadbd7726a23829d8d6af27a34a14b5a
CeeFish <can.o.fisher@gmail.com>
2022-05-02 20:04:13 -0400

    Pig latin file finished and passing with if else statements.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,22 +1,37 @@
 #write your code here
-class PigLatin
-    
-    def pig_latin(words)
-        first_step = words.split(" ")
-        second_step = a.map {|string| pig_latin_word(string)}
-        second_step.join(" ")
+def translate (pig_latin)
+    words = pig_latin.split(" ")
+    new_words = words.map do |word|
+        translate_one_word (word)
     end
+    @translate = new_words.join(" ")
+end
 
-    def translate (string)
-        first_letter = string[0].downcase
-        consonants = []
+def translate_one_word(pig_latin)
+    first_letter = pig_latin[0].downcase #letter index
 
-        if ["a", "e", "i", "o", "u"].include?(first_letter)
-            return "#{string}ay"
-        else 
-            return if ["a", "e", "i", "o", "u"].include?(string[1, 2]) == false
-        end
-    "#{string[consonants.length..-1] + consonants.join + "ay"}"
+    if is_a_vowel(first_letter) # if it includes these
+        return "#{pig_latin}ay"
+    elsif pig_latin[0] == "q" && pig_latin[1] == "u"
+        pig_latin[2..-1] + pig_latin[0..1] + "ay"
+    elsif is_a_consonant(pig_latin[0]) && pig_latin[1] == "q" && pig_latin[2] == "u"
+        pig_latin[3..-1] + pig_latin[0..2] + "ay"
+    elsif is_a_consonant(pig_latin[0]) && is_a_consonant(pig_latin[1]) && is_a_consonant(pig_latin[2])
+        pig_latin[3..-1] + pig_latin[0..2] + "ay"
+    elsif is_a_consonant(pig_latin[0]) && is_a_consonant(pig_latin[1]) # if it includeds consonants in index 0 and 1
+        pig_latin[2..-1] + pig_latin[0..1] + "ay"
+    else is_a_consonant(pig_latin[0]) # if it includeds consonants in index 0
+        pig_latin[1..-1] + pig_latin[0] + "ay"
     end
 end
 
+def is_a_vowel(letter)
+    vowels = ["a", "e", "i", "o", "u"]
+    vowels.include? (letter)
+end
+
+def is_a_consonant(letter)
+    vowels = ["a", "e", "i", "o", "u"]
+    consonants = ('a'..'z').to_a - vowels
+    consonants.include? (letter)
+end
