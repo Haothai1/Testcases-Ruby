ff71d7e4e1c29bd115ed78d28307d9ea207009de
CeeFish <can.o.fisher@gmail.com>
2022-05-02 20:40:51 -0400

    Add comments to my code

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -8,14 +8,14 @@ class Book
 
         words = book_title.split(" ") # words variable will split the book title words into an array
         new_words = words.map.with_index do |word, index| # map allows you to apply a function to every element of a data structure and receive the result
-            if index == 0 #if the index is 1st 
+            if index == 0 #if the index is eqaul to then capitlize the word
                 word.capitalize()
-            elsif articles.include? word # if articles are included in articles than return those words
+            elsif articles.include? word # if the word is one of the articles then just return the word
                 word
             else
-                word.capitalize() #if it does not include articles than capitalize words
+                word.capitalize() #if the word in the title is not one of the articles words then return the word capitalized
             end
         end
-        @title = new_words.join(" ")
+        @title = new_words.join(" ") # join the array that was split back together with space in between the title
     end
 end
