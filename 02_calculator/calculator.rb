78373376941d651b82dadca09339c0249dbcb2b0
Cowboy <cbrobak79@gmail.com>
2022-12-25 04:12:07 -0500

    Week 5 assignment

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+def add(num1, num2)
+    num1 + num2
+end
+
+def subtract(num1, num2)
+    num1 - num2
+end
+
+def sum(array)
+    array.sum
+end
+
+def multiply(args)
+    sum = 1
+    args.each { |el| sum *= el }
+    sum
+end
+
+def power(num, power)
+    sum = 0
+    power.times do
+       sum = num * num
+    end
+    sum
+end
+
+def factorial(num)
+    array = (1..num).to_a
+    sum = 1
+    array.reverse.each { |el| sum *= el }
+    sum
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
 #write your code here
+def translate(str)
+    words = str.split
+    pig_latin_words = words.map do |word|
+      if word[0] =~ /[aeiouAEIOU]/
+        word + "ay"
+      else
+        first_vowel_index = word.index(/[aeiouAEIOU]/)
+        if word[first_vowel_index - 1, 2] == "qu"
+          word[first_vowel_index + 1..-1] + word[0, first_vowel_index + 1] + "ay"
+        else
+          word[first_vowel_index..-1] + word[0, first_vowel_index] + "ay"
+        end
+      end
+    end
+    pig_latin_words.join(" ")
+  end
+
+
