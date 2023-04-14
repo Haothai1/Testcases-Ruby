63575ab44ccaf9bf36c05918f0b0c310edf1b36e
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2023-04-14 19:21:33 -0400

    testing message

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,30 @@
 #write your code here
+
+
+def translate (string)
+
+    alpha = ('a'..'z').to_a
+
+    vowels = %w[a e i o u]
+
+    consonants = alpha - vowels
+  
+    if vowels.include?(string[0])
+
+      string + 'ay'
+
+    elsif consonants.include?(string[0]) && consonants.include?(string[1])
+
+      string[2..-1] + string[0..1] + 'ay'
+
+    elsif consonants.include?(string[0])
+
+      string[1..-1] + string[0] + 'ay'
+
+    else
+
+      string
+
+    end
+
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,33 @@
 class Book
-# write your code here
+
+    attr_reader :title
+    attr_writer :title
+
+    def title=(title)
+
+        @title = title
+        
+        @title = @title.capitalize!
+
+        words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but", "in", "of", "an"]
+    
+        result = @title.split(" ").map {|word| 
+    
+            if words_no_cap.include?(word) 
+                
+                word
+    
+            else
+    
+                word.capitalize
+    
+            end
+    
+        }.join(" ") 
+        
+        @title = result 
+    
+    
+    end
+
 end
