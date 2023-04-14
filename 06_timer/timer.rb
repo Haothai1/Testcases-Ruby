254d4e732c0e743d334a0bdf5d158da196996b99
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2023-04-14 19:21:33 -0400

    testing message

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,30 @@
+
 class Timer
-  #write your code here
+
+  attr_accessor :seconds
+
+  def initialize
+
+    @seconds = 0
+  
+  end
+
+  def padded i
+
+    "#{i}".rjust(2, '0')
+    
+  end
+
+  def time_string
+    
+    hours = @seconds / 3600
+
+    mins = (@seconds % 3600) / 60
+
+    seconds = @seconds % 60
+
+    @time_string = [hours, mins, seconds].map(&method(:padded)).join(":")  
+
+  end
+
 end
