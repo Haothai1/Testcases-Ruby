cedde8498baece0c0fb15714ec53fd2e9dff29a1
Hetal03 <126925831+Hetal03@users.noreply.github.com>
2023-05-12 11:06:01 -0500

    Add files via upload

---
+++ Backend-ruby-testfirst-master/03_simon_says/simon_says.rb
@@ -0,0 +1,41 @@
+#write your code here
+def echo(input)
+    input
+end
+
+def shout(input)
+    input.upcase
+end
+
+def repeat(string, times = 2)
+    repeated_string = ""
+    times.times do
+      repeated_string += string + " "
+    end
+    repeated_string.strip
+end
+
+def start_of_word(word, n)
+    word.slice(0, n)
+  end
+  
+  def first_word(sentence)
+    sentence.split.first
+  end
+  
+
+  def titleize(title)
+    words = title.split
+    little_words = ["and", "the", "over"]
+    titleized_words = []
+    
+    words.each_with_index do |word, index|
+      if index == 0 || !little_words.include?(word)
+        titleized_words << word.capitalize
+      else
+        titleized_words << word
+      end
+    end
+    
+    titleized_words.join(" ")
+  end
